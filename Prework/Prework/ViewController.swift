8c3f0c7f1df27ee13331f1ad864e218637b6c2e1
johnxminimo <78349183+johnxminimo@users.noreply.github.com>
2022-01-05 14:01:16 -0800

    added swift project files

---
+++ Prework/Prework/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Prework
+//
+//  Created by John Minimo on 12/27/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var billAmountTextField: UILabel!
+    @IBOutlet weak var tipAmountLabel: UILabel!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func calculateTip(_ sender: Any) {
+        
+    }
+    
+}
+
