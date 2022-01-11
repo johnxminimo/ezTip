8c12ad0be2362e154fe569a7c16d4dd211e6618a
johnxminimo <78349183+johnxminimo@users.noreply.github.com>
2022-01-10 20:38:10 -0800

    v2 of program, created splt bill counter

--- Prework/Prework/ViewController.swift
+++ Prework/Prework/ViewController.swift
@@ -13,31 +13,47 @@ class ViewController: UIViewController {
     @IBOutlet weak var tipAmountLabel: UILabel!
     @IBOutlet weak var tipControl: UISegmentedControl!
     @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var totalPerPerson: UILabel!
+    @IBOutlet weak var stepper: UIStepper!
+    @IBOutlet weak var stepperCount: UILabel!
+    @IBOutlet weak var stepperCounter: UILabel!
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        stepper.wraps = true
+        stepper.autorepeat = true
+        stepper.maximumValue = 10
     }
 
     @IBAction func calculateTip(_ sender: Any) {
+        
         // Gets billAmount value
         let billAmount:Double = Double(self.billAmountTextField.text!) ?? 0.0
         let tipPercentages:[Double] = [0.15, 0.18, 0.2]
         // Calculate tip by multiplying billAmount by selected tipPercentage
         let tip = billAmount*tipPercentages[tipControl.selectedSegmentIndex]
-        
+
         
         // Calculate total by adding tip + billAmount
         let total = tip+billAmount
+        let personCount:Double = Double(self.stepperCounter.text!) ?? 0.0
+        
+        let totalPerP = total/personCount
+        
         
         // Updates text labels with correct toatls
         tipAmountLabel.text = String( format: "$%.2f", tip)
         totalLabel.text =  String(format: "$%.2f",total)
+        totalPerPerson.text = String(format:"Total Per Person: $%.2f", totalPerP)
         
         
         
         
+    }
+    @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        stepperCounter.text = Int(sender.value).description
     }
     
 }
